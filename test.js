a2c301de963006e7659d10bec991fbbc77c19bdf
Dnld <steinert.donald@gmail.com>
2015-09-14 23:27:07 -0400

    implements encrypted psaswords, passes all tests, all pages load properly

--- test.js
+++ test.js
@@ -1,40 +1,14 @@
+var bcrypt = require('bcrypt-nodejs');
 
+var hashB = bcrypt.hashSync('cheezits', null, null);
 
-// function restrict(req, res, next) {
-//   if (req.session.user) {
-//     next();
-//   } else {
-//     req.session.error = 'Access denied!';
-//     res.redirect('/login');
-//   }
-// }
-
-
-
-app.get('/login',
-function(req, res) {
-  if (loggedIn) {
-    res.redirect('/');
-  } else {
-    res.render('/login');
+bcrypt.compare('cheezits', hashB, function(err, result) {
+  if (err) {
+    console.log(err);
   }
-});
-
-app.post('/login',
-function(req, res) {
-  var user = req.body.username;
-  var pass = req.body.password;
-  new User({ username: user, password: pass })
-    .fetch()
-    .then(function(found) {
-      if (found) {
-        req.session.loggedIn = true;
-        res.redirect('/');
-        // user is logged in
-        // redirect to '/'
-      } else {
-        res.redirect('/');
-        // redirect to '/login'
-      }
-  });
+  if (result) {
+    console.log('success');
+  } else {
+    console.log('whyyyyyy');
+  }
 });
